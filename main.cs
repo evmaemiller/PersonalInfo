7f437151bc78123502615d85c79f2b6a44816628
Eva Mae Miller <150403453+evmaemiller@users.noreply.github.com>
2024-05-26 00:40:26 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Lucinda Potter");
+    Console.WriteLine ("6/24/1992");
+    Console.WriteLine ("work 000-000-0101");
+    Console.WriteLine ("cell 000-000-0189");
+  }
+}
