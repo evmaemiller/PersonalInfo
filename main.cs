9c2d6b4d319d3566a9371723a6b50d813d16dfd9
Eva Mae Miller <150403453+evmaemiller@users.noreply.github.com>
2024-05-26 00:49:29 +0000

    For Unit Test #4, I changed information to display that of Robert Hall's.

--- main.cs
+++ main.cs
@@ -2,9 +2,9 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Chris Johnson");
-    Console.WriteLine ("12/01/1987");
-    Console.WriteLine ("work 000-222-0101");
-    Console.WriteLine ("cell 000-222-0189");
+    Console.WriteLine ("Robert Hall");
+    Console.WriteLine ("2/27/1949");
+    Console.WriteLine ("work 000-444-0101");
+    Console.WriteLine ("cell 000-444-0189");
   }
 }
