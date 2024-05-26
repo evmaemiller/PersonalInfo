0bf817422c13e9bd7911d8e3d182c2b510a9c17a
Eva Mae Miller <150403453+evmaemiller@users.noreply.github.com>
2024-05-26 00:44:54 +0000

    For Unit Test #2 , I changed information to display that of John Smith's.

--- main.cs
+++ main.cs
@@ -2,9 +2,9 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Lucinda Potter");
-    Console.WriteLine ("6/24/1992");
-    Console.WriteLine ("work 000-000-0101");
-    Console.WriteLine ("cell 000-000-0189");
+    Console.WriteLine ("John Smith");
+    Console.WriteLine ("8/15/1978");
+    Console.WriteLine ("work 000-111-0101");
+    Console.WriteLine ("cell 000-111-0189");
   }
 }
